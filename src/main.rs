1d0ddb35a96825030a453521f079169568d28c5b
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2023-12-15 15:19:32 +0900

    Part 1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use solutions::*;
 use std::fs;
 use std::time;
 
-type Today = Day14;
+type Today = Day15;
 
 fn main() {
     let input = read_input("input.txt");
--- src/solutions/day15.rs
+++ src/solutions/day15.rs
@@ -1,20 +1,29 @@
-
 use super::Solution;
 
 pub struct Day15;
 
 impl Solution for Day15 {
     fn test_input() -> String {
-        String::from("")
+        String::from("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
     }
 
-    fn solve_part_1(_input: String) -> String {
-        String::from("0")
+    fn solve_part_1(input: String) -> String {
+        input
+            .split(",")
+            .map(|step| hash(step))
+            .sum::<u32>()
+            .to_string()
     }
 
     fn solve_part_2(_input: String) -> String {
         String::from("0")
     }
+}
+
+fn hash(s: &str) -> u32 {
+    s.chars()
+        .map(|ch| ch as u8)
+        .fold(0u8, |acc, now| acc.wrapping_add(now).wrapping_mul(17)) as u32
 }
 
 #[cfg(test)]
@@ -25,7 +34,7 @@ mod day15_tests {
     fn test_part_1() {
         let input = Day15::test_input();
         let ans = Day15::solve_part_1(input);
-        assert_eq!(ans, "");
+        assert_eq!(ans, "1320");
     }
 
     #[test]
