37c24a6bc94461ac61cc8183d52af5aa1231ffa2
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2023-12-18 20:30:35 +0900

    Part 2

--- src/solutions/day18.rs
+++ src/solutions/day18.rs
@@ -2,8 +2,6 @@ use std::str::FromStr;
 
 use super::Solution;
 
-use super::utils::Pos;
-
 pub struct Day18;
 
 impl Solution for Day18 {
@@ -27,72 +25,68 @@ impl Solution for Day18 {
     }
 
     fn solve_part_1(input: String) -> String {
-        let mut points = vec![(0, 0)];
+        let mut prev = Pos(0, 0);
+        let mut points = vec![prev];
+        let mut perimeter = 0;
         for line in input.lines() {
             let (dir, dist) = parse(line);
-            let p = *points.last().unwrap();
-
-            match dir {
-                Dir::Down => {
-                    for i in 1..=dist {
-                        points.push((p.0 + i, p.1));
-                    }
-                }
-                Dir::Up => {
-                    for i in 1..=dist {
-                        points.push((p.0 - i, p.1));
-                    }
-                }
-                Dir::Right => {
-                    for i in 1..=dist {
-                        points.push((p.0, p.1 + i));
-                    }
-                }
-                Dir::Left => {
-                    for i in 1..=dist {
-                        points.push((p.0, p.1 - i));
-                    }
-                }
-            }
-        }
-        let min_x = points.iter().map(|(x, _)| x).min().unwrap();
-        let max_x = points.iter().map(|(x, _)| x).max().unwrap();
-        let min_y = points.iter().map(|(_, y)| y).min().unwrap();
-        let max_y = points.iter().map(|(_, y)| y).max().unwrap();
-        let mut grid: Vec<Vec<Option<bool>>> = (0..=(max_x - min_x + 3))
-            .map(|_| (0..(max_y - min_y + 3)).map(|_| Some(false)).collect())
-            .collect();
-
-        for point in points.iter() {
-            let (x, y) = point;
-            let x = x - min_x + 1;
-            let y = y - min_y + 1;
-            let x = x as usize;
-            let y = y as usize;
-            grid[x][y] = Some(true);
+            perimeter += dist;
+            let pos = match dir {
+                Dir::Right => Pos(prev.0, prev.1 + dist),
+                Dir::Left => Pos(prev.0, prev.1 - dist),
+                Dir::Up => Pos(prev.0 - dist, prev.1),
+                Dir::Down => Pos(prev.0 + dist, prev.1),
+            };
+            points.push(pos);
+            prev = pos;
         }
 
-        let mut stack = vec![Pos(0, 0)];
-        grid[0][0] = None;
-        let clamp = Pos(grid.len(), grid[0].len());
-        while let Some(pos) = stack.pop() {
-            for np in pos.next(&clamp) {
-                if let Some(false) = grid[np.0][np.1] {
-                    grid[np.0][np.1] = None;
-                    stack.push(np);
-                }
-            }
-        }
-        grid.iter()
-            .map(|row| row.iter().filter_map(|a| *a).count())
-            .sum::<usize>()
-            .to_string()
+        let area = get_area(&points);
+
+        let internal = area + 1 - perimeter / 2;
+
+        (internal + perimeter).to_string()
     }
 
     fn solve_part_2(input: String) -> String {
-        String::from("0")
+        let mut prev = Pos(0, 0);
+        let mut points = vec![prev];
+        let mut perimeter = 0;
+        for line in input.lines() {
+            let (dir, dist) = parse_line_2(line);
+            perimeter += dist;
+            let pos = match dir {
+                Dir::Right => Pos(prev.0, prev.1 + dist),
+                Dir::Left => Pos(prev.0, prev.1 - dist),
+                Dir::Up => Pos(prev.0 - dist, prev.1),
+                Dir::Down => Pos(prev.0 + dist, prev.1),
+            };
+            points.push(pos);
+            prev = pos;
+        }
+
+        let area = get_area(&points);
+
+        let internal = area + 1 - perimeter / 2;
+
+        (internal + perimeter).to_string()
     }
 }
+
+fn get_area(points: &[Pos]) -> i64 {
+    let mut ans = 0;
+    for j in 1..points.len() {
+        let i = j - 1;
+        let Pos(x1, y1) = points[i];
+        let Pos(x2, y2) = points[j];
+        ans += x1 * y2 - x2 * y1;
+    }
+    ans /= 2;
+    ans.abs()
+}
+
+#[derive(Clone, Copy)]
+struct Pos(i64, i64);
 
 fn parse(line: &str) -> (Dir, i64) {
     let mut words = line.trim().split_whitespace();
@@ -100,6 +94,20 @@ fn parse(line: &str) -> (Dir, i64) {
         words.next().unwrap().parse().unwrap(),
         words.next().unwrap().parse().unwrap(),
     )
+}
+
+fn parse_line_2(line: &str) -> (Dir, i64) {
+    let hex = line.trim().split_whitespace().nth(2).unwrap();
+    let dist = i64::from_str_radix(&hex[2..7], 16).unwrap();
+    let dir = match hex.chars().nth(7).unwrap() {
+        //RDLU
+        '0' => Dir::Right,
+        '1' => Dir::Down,
+        '2' => Dir::Left,
+        '3' => Dir::Up,
+        _ => panic!("sth wrong"),
+    };
+    (dir, dist)
 }
 
 enum Dir {
@@ -138,6 +146,6 @@ mod day18_tests {
     fn test_part_2() {
         let input = Day18::test_input();
         let ans = Day18::solve_part_2(input);
-        assert_eq!(ans, "");
+        assert_eq!(ans, "952408144115");
     }
 }
