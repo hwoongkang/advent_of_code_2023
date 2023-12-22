e0afd4148e2886e58178e59692ec9c78903d00b1
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2023-12-22 17:38:33 +0900

    topological sort bug fix && Part 1

--- src/solutions/day22.rs
+++ src/solutions/day22.rs
@@ -1,7 +1,7 @@
-use std::collections::VecDeque;
-use std::{cmp::Ordering, collections::HashMap, ops::SubAssign, str::FromStr};
+use std::collections::{HashSet, VecDeque};
+use std::{cmp::Ordering, ops::SubAssign, str::FromStr};
 
-use std::ops::Range as StdRange;
+use std::ops::{Range as StdRange, Sub};
 
 use super::Solution;
 
@@ -10,29 +10,68 @@ pub struct Day22;
 impl Solution for Day22 {
     fn test_input() -> String {
         String::from(
-            "1,0,1~1,2,1
+            "1,1,8~1,1,9
+        1,0,1~1,2,1
         0,0,2~2,0,2
         0,2,3~2,2,3
         0,0,4~0,2,4
         2,0,5~2,2,5
-        0,1,6~2,1,6
-        1,1,8~1,1,9",
+        0,1,6~2,1,6",
         )
     }
 
     fn solve_part_1(input: String) -> String {
         let mut bricks: Vec<Brick> = input.lines().map(|line| line.parse().unwrap()).collect();
 
-        let (edges, topo) = topological_sort(&bricks);
+        let (under, over, topo) = collapse_bricks(&mut bricks);
 
-        println!("{:?}", edges.iter().map(|e| e.len()).collect::<Vec<_>>());
+        let mut supported_by: Vec<Vec<usize>> = bricks.iter().map(|_| vec![]).collect();
 
-        String::from("0")
+        for i in 0..bricks.len() {
+            let me = &bricks[i];
+            for j in over[i].iter() {
+                let under_me = &bricks[*j];
+                if under_me.distance(me) == 0 {
+                    supported_by[i].push(*j);
+                }
+            }
+        }
+
+        let mut crucial: HashSet<usize> = HashSet::new();
+
+        for supports in supported_by {
+            if supports.len() == 1 {
+                crucial.insert(supports[0]);
+            }
+        }
+        (bricks.len() - crucial.len()).to_string()
     }
 
     fn solve_part_2(_input: String) -> String {
         String::from("0")
     }
+}
+
+fn collapse_bricks(bricks: &mut [Brick]) -> (Vec<Vec<usize>>, Vec<Vec<usize>>, Vec<usize>) {
+    let (under, over, topo) = topological_sort(&bricks);
+    for i in 0..bricks.len() {
+        let i = topo[i];
+
+        if over[i].len() == 0 {
+            let brick = &mut bricks[i];
+            *brick -= Pos(0, 0, brick.bottom() - 1);
+        } else {
+            let brick = &bricks[i];
+            let dist = over[i]
+                .iter()
+                .map(|j| bricks[*j].distance(brick))
+                .min()
+                .unwrap();
+            let brick = &mut bricks[i];
+            *brick -= Pos(0, 0, dist);
+        }
+    }
+    (under, over, vec![])
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
@@ -46,17 +85,10 @@ impl SubAssign for Pos {
     }
 }
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
 struct Brick {
     start: Pos,
     end: Pos,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
-enum Dir {
-    X,
-    Y,
-    Z,
 }
 
 impl SubAssign<Pos> for Brick {
@@ -91,45 +123,6 @@ impl FromStr for Brick {
 }
 
 impl Brick {
-    fn dir(&self) -> Dir {
-        let Pos(x0, y0, z0) = self.start;
-        let Pos(x1, y1, z1) = self.end;
-        if z0 != z1 {
-            Dir::Z
-        } else if x0 != x1 {
-            Dir::X
-        } else if y0 != y1 {
-            Dir::Y
-        } else {
-            Dir::Z
-        }
-    }
-    fn bottom(&self) -> Vec<Pos> {
-        let dir = self.dir();
-        let Pos(x0, y0, z0) = self.start;
-        let Pos(x1, y1, z1) = self.end;
-        match dir {
-            Dir::Z => vec![Pos(x0, y0, z0.min(z1))],
-            Dir::X => (x0.min(x1)..=x0.max(x1)).map(|x| Pos(x, y0, z0)).collect(),
-            Dir::Y => (y0.min(y1)..=y0.max(y1)).map(|y| Pos(x0, y, z0)).collect(),
-        }
-    }
-
-    fn top(&self) -> Vec<Pos> {
-        let dir = self.dir();
-        let Pos(x0, y0, z0) = self.start;
-        let Pos(x1, y1, z1) = self.end;
-        match dir {
-            Dir::Z => vec![Pos(x0, y0, z0.max(z1) + 1)],
-            Dir::X => (x0.min(x1)..=x0.max(x1))
-                .map(|x| Pos(x, y0, z0 + 1))
-                .collect(),
-            Dir::Y => (y0.min(y1)..=y0.max(y1))
-                .map(|y| Pos(x0, y, z0 + 1))
-                .collect(),
-        }
-    }
-
     fn as_range(&self) -> (Range, Range, Range) {
         (
             Range::from(self.start.0, self.end.0),
@@ -190,10 +183,12 @@ impl IntoIterator for Range {
 fn topological_sort(
     bricks: &[Brick],
 ) -> (
-    Vec<Vec<usize>>, // relations
+    Vec<Vec<usize>>, // shows which indices are above me
+    Vec<Vec<usize>>, // show which indices are under me
     Vec<usize>,      //result
 ) {
-    let mut edges: Vec<Vec<usize>> = bricks.iter().map(|_| vec![]).collect();
+    let mut under: Vec<Vec<usize>> = bricks.iter().map(|_| vec![]).collect();
+    let mut over: Vec<Vec<usize>> = bricks.iter().map(|_| vec![]).collect();
     let len = bricks.len();
     for i in 0..(len - 1) {
         for j in (i + 1)..len {
@@ -201,8 +196,14 @@ fn topological_sort(
             let rhs = &bricks[j];
             match lhs.partial_cmp(rhs) {
                 None => {}
-                Some(Ordering::Less) => edges[i].push(j),
-                Some(Ordering::Greater) => edges[j].push(i),
+                Some(Ordering::Less) => {
+                    under[i].push(j);
+                    over[j].push(i);
+                }
+                Some(Ordering::Greater) => {
+                    under[j].push(i);
+                    over[i].push(j);
+                }
                 _ => unreachable!(),
             }
         }
@@ -213,6 +214,7 @@ fn topological_sort(
     let mut ans: VecDeque<usize> = VecDeque::new();
 
     fn dfs(from: usize, edges: &[Vec<usize>], visited: &mut [bool], ans: &mut VecDeque<usize>) {
+        visited[from] = true;
         for &next in edges[from].iter() {
             if !visited[next] {
                 visited[next] = true;
@@ -224,16 +226,47 @@ fn topological_sort(
 
     for i in 0..len {
         if !visited[i] {
-            dfs(i, &edges, &mut visited, &mut ans);
+            dfs(i, &under, &mut visited, &mut ans);
         }
     }
 
-    (edges, Vec::from(ans))
+    (under, over, Vec::from(ans))
+}
+
+impl Brick {
+    fn top(&self) -> usize {
+        self.start.2.max(self.end.2)
+    }
+    fn bottom(&self) -> usize {
+        self.start.2.min(self.end.2)
+    }
+
+    fn distance(&self, up: &Self) -> usize {
+        if !(self <= up) {
+            unreachable!()
+        }
+        up.bottom() - self.top() - 1
+    }
 }
 
 #[cfg(test)]
 mod day22_tests {
     use super::*;
+
+    #[test]
+    fn test_collapse() {
+        let input = Day22::test_input();
+        let mut bricks: Vec<Brick> = input.lines().map(|line| line.parse().unwrap()).collect();
+        let mut original: Vec<Brick> = input.lines().map(|line| line.parse().unwrap()).collect();
+        let _ = collapse_bricks(&mut bricks);
+        let fall_dist: [usize; 7] = [3, 0, 0, 1, 1, 2, 2];
+        for i in 0..7 {
+            let brick = &mut original[i];
+            *brick -= Pos(0, 0, fall_dist[i]);
+
+            assert_eq!(*brick, bricks[i]);
+        }
+    }
 
     #[test]
     fn test_part_1() {
@@ -250,17 +283,6 @@ mod day22_tests {
     }
 
     #[test]
-    fn test_top_bottom() {
-        let a: Brick = "1,0,1~1,2,1".parse().unwrap();
-        assert_eq!(a.top(), vec![Pos(1, 0, 2), Pos(1, 1, 2), Pos(1, 2, 2)]);
-        assert_eq!(a.bottom(), vec![Pos(1, 0, 1), Pos(1, 1, 1), Pos(1, 2, 1)]);
-
-        let g: Brick = "1,1,8~1,1,9".parse().unwrap();
-        assert_eq!(g.top(), vec![Pos(1, 1, 10)]);
-        assert_eq!(g.bottom(), vec![Pos(1, 1, 8)]);
-    }
-
-    #[test]
     fn test_partial_ord() {
         let a: Brick = "1,0,1~1,2,1".parse().unwrap();
         let b: Brick = "1,1,8~1,1,9".parse().unwrap();
@@ -274,25 +296,26 @@ mod day22_tests {
     fn test_topological_sort() {
         let input = Day22::test_input();
         let bricks: Vec<Brick> = input.lines().map(|line| line.parse().unwrap()).collect();
-        let (edges, ans) = topological_sort(&bricks);
+        let (under, _, ans) = topological_sort(&bricks);
+        // G A B C D E F
         assert_eq!(
-            edges,
+            under,
             vec![
-                vec![1, 2, 5, 6],
-                vec![3, 4],
-                vec![3, 4],
-                vec![5],
-                vec![5],
+                vec![],
+                vec![0, 2, 3, 6],
+                vec![4, 5],
+                vec![4, 5],
                 vec![6],
-                vec![]
+                vec![6],
+                vec![0]
             ]
         );
 
         assert!(
-            ans == [0, 1, 2, 3, 4, 5, 6]
-                || ans == [0, 2, 1, 3, 4, 5, 6]
-                || ans == [0, 1, 2, 4, 3, 5, 6]
-                || ans == [0, 2, 1, 4, 3, 5, 6]
+            ans == [1, 2, 3, 4, 5, 6, 0]
+                || ans == [1, 3, 2, 4, 5, 6, 0]
+                || ans == [1, 2, 3, 5, 4, 6, 0]
+                || ans == [1, 3, 2, 5, 4, 6, 0]
         );
     }
 }
